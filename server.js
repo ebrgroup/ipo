d2346b4180a8d784dea04a1d7f629ab63d704b0c
EBR <ebrgroup@proton.me>
2024-01-25 19:23:42 +0500

    Chaning server.js file

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use("/ipo", authenticationRoute);
 app.use("/ipo", authMiddleware, userRoutes);
 
 // Serve static files from the React app's build directory
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static('client/build'));
 
 // Handle requests to your React app's HTML file
-app.all('*', (req, res) => {
-    res.sendfile(path.join(__dirname, './client/build/index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
 // Start the server
