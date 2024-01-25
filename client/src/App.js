7b8469b52b2e320c573fe93fdf88e828fe0e58e4
EBR <ebrgroup@proton.me>
2024-01-25 19:04:50 +0500

    Testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,14 @@
-import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import axios from "axios";
 import './App.css';
 
-const App = () => {
-
-  const [userData, setUserData] = useState({
-    userId: null,
-    token: null
-  });
-
-  useEffect(() => {
-    (async () => {
-      await axios.post("/ipo/login", {
-        cnic: "1234567890123",
-        password: "secretpassword"
-      })
-      .then(response => {
-        setUserData({
-          userId: response.data._id,
-          token: response.data.token
-        });
-      }).catch(error => {
-        console.error(error);
-      })
-    })();
-  }, []);
-  
+function App() {
 
   return (
     <div className="App">
       <h1>Following data is coming from database:</h1>
       <h2>User Id: </h2>
-      <h3>{userData.userId}</h3>
+      <h3>Haider</h3>
     </div>
   );
 }
