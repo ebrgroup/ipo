5a0f2c45788eab538acacddbe07701937b241df8
EBR <ebrgroup@proton.me>
2024-01-25 19:26:15 +0500

    Chaning server.js file

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use("/ipo", authMiddleware, userRoutes);
 app.use(express.static('client/build'));
 
 // Handle requests to your React app's HTML file
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
